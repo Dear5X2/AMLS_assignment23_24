54929eb400560fae597fa970e52d81a58b01660d
Pengbo Xiang <114840551+Dear5X2@users.noreply.github.com>
2024-01-20 00:55:40 +0000

    Update main.py

--- AMLS_23-24_SN20019229/main.py
+++ AMLS_23-24_SN20019229/main.py
@@ -1,13 +1,35 @@
+import os
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-def run_notebook(notebook_path):
-    with open(notebook_path) as file:
+# Function to run a notebook
+def run_notebook(notebook_directory, notebook_name):
+    # Construct the full path to the notebook
+    notebook_path = os.path.join(notebook_directory, notebook_name)
+    
+    # Load the notebook
+    with open(notebook_path, 'r', encoding='utf-8') as file:
         nb = nbformat.read(file, as_version=4)
+        
+        # Create an execute preprocessor instance
         ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-        ep.preprocess(nb, {'metadata': {'path': './'}})
+        
+        # Execute the notebook
+        ep.preprocess(nb, {'metadata': {'path': notebook_directory}})
+        
+        # Save the executed notebook
+        with open(notebook_path, 'w', encoding='utf-8') as file:
+            nbformat.write(nb, file)
 
 if __name__ == "__main__":
-    run_notebook('PneumoniaMNIST.ipynb')
-    run_notebook('PathMNIST.ipynb')
-
+    # Paths to the notebooks
+    pneumonia_notebook_directory = 'A'
+    pneumonia_notebook_name = 'PneumoniaMNIST.ipynb'
+    
+    path_notebook_directory = 'B'
+    path_notebook_name = 'PathMNIST.ipynb'
+    
+    # Run the notebooks
+    run_notebook(pneumonia_notebook_directory, pneumonia_notebook_name)
+    
+    run_notebook(path_notebook_directory, path_notebook_name)
